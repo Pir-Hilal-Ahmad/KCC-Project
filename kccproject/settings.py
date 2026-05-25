f56e0e1c035d142e8a69166909341034ef14f4b1
PIR HILAL AHMAD <73270793+Pir-Hilal-Ahmad@users.noreply.github.com>
2026-05-25 21:59:54 +0530

    Update settings.py

--- kccproject/settings.py
+++ kccproject/settings.py
@@ -119,7 +119,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
@@ -132,3 +131,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
